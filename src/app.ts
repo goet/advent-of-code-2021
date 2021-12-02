63eacd54d5236dc4c0f6252f86fe39cedd905dd9
goet <vissiadam@gmail.com>
2021-12-02 19:25:44 +0100

    refactor solution class

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,4 @@ import { DayOne } from './day01/day-one';
 import { DistanceChecker } from './day01/distance-checker';
 
 var dayOne = new DayOne();
-dayOne.run();
+dayOne.runA();
--- src/day01/day-one.ts
+++ src/day01/day-one.ts
@@ -1,4 +1,4 @@
-import { readFile, readFileSync } from 'fs';
+import { readFileSync } from 'fs';
 import { DistanceChecker } from './distance-checker';
 
 export class DayOne {
@@ -6,11 +6,17 @@ export class DayOne {
 
     private measurements: number[] = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
-    run() {
+    runA() {
         this.parseData('src/day01/data.txt');
         const relativeChanges = this.distanceChecker.checkRelativeChanges(this.measurements);
         const result = this.countIncreases(relativeChanges);
-        console.log(result);
+        console.log(`solution complete: ${result}`);
+    }
+
+    testA() {
+        const relativeChanges = this.distanceChecker.checkRelativeChanges(this.measurements);
+        const result = this.countIncreases(relativeChanges);
+        console.log(`test complete: ${result} expected: 7`);
     }
 
     private parseData(path: string) {
